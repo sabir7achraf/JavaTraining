e6570b9d15d24125c10820b7f89ed08e0d566522
sabir7achraf <sabir.achraf@etu.uae.ac.ma>
2025-02-13 01:27:42 +0100

    Insertion Sort

--- SortingAndSearchingAlgorithms/InsertionSort.java
+++ SortingAndSearchingAlgorithms/InsertionSort.java
@@ -10,7 +10,7 @@ public class InsertionSort {
      for (int i = 1; i < n; i++) {
         int key = arr[i];
         int j = i - 1;
-        while (j >= 0 && arr[j] > key) {
+        while (j >= 0 && arr[j] > key) { //for shifting element
             arr[j + 1] = arr[j];
             j = j - 1;
         }
