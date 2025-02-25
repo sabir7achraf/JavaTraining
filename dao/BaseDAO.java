0e9d24a6f99a9fbbb638cefcdffc4360f10a9b54
sabir7achraf <sabir.achraf@etu.uae.ac.ma>
2025-02-25 20:21:32 +0000

    BaseDao

--- dao/BaseDAO.java
+++ dao/BaseDAO.java
@@ -1,36 +1,36 @@
-package ma.ecole.dao;
-
-import ma.ecole.tools.ConnectionBDD;
-import ma.ecole.tools.ConnectionFactory;
-
-import java.sql.*;
-import java.util.List;
-
-public abstract  class BaseDAO <T> {
-
-    // attributs
-
-   protected  Connection myconnection ;
-    protected Statement statement ;
-    protected  PreparedStatement preparedStatement ;
-    protected  ResultSet resultSet ;
-
-    // params de connection
-
-
-
-// constrecteur par defaut
-    public BaseDAO() throws SQLException {
-
-        this.myconnection = ConnectionFactory.getConnection("POSTGRES");
-    }
-
-
-
-    public abstract  void save( T obj ) throws SQLException;
-    public abstract  void update( T obj ) throws SQLException;
-    public abstract  void delete( T obj ) throws SQLException;
-    public abstract  T getOne( int id ) throws SQLException;
-    public abstract List<T> getAll( ) throws SQLException;
-
-}
+//package ma.ecole.dao;
+//
+//import ma.ecole.tools.ConnectionBDD;
+//import ma.ecole.tools.ConnectionFactory;
+//
+//import java.sql.*;
+//import java.util.List;
+//
+//public abstract  class BaseDAO <T> {
+//
+//    // attributs
+//
+//   protected  Connection myconnection ;
+//    protected Statement statement ;
+//    protected  PreparedStatement preparedStatement ;
+//    protected  ResultSet resultSet ;
+//
+//    // params de connection
+//
+//
+//
+//// constrecteur par defaut
+//    public BaseDAO() throws SQLException {
+//
+//        this.myconnection = ConnectionFactory.getConnection("POSTGRES");
+//    }
+//
+//
+//
+//    public abstract  void save( T obj ) throws SQLException;
+//    public abstract  void update( T obj ) throws SQLException;
+//    public abstract  void delete( T obj ) throws SQLException;
+//    public abstract  T getOne( int id ) throws SQLException;
+//    public abstract List<T> getAll( ) throws SQLException;
+//
+//}
